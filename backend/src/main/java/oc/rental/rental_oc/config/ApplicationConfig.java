c6e37e1cfa5166daf42745040b8d512dea4cb851
The-Great-Toad <bearcode26@gmail.com>
2025-08-08 13:10:07 +0200

    feat (rentals): implement endpoint post /rentals
    - missing file in commit

--- backend/src/main/java/oc/rental/rental_oc/config/ApplicationConfig.java
+++ backend/src/main/java/oc/rental/rental_oc/config/ApplicationConfig.java
@@ -2,10 +2,12 @@ package oc.rental.rental_oc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableJpaAuditing
 public class ApplicationConfig {
 
     @Bean
